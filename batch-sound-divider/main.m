9a4eb7aecac45f25e8e19052eb930e8bbec24ed4
Yuri Edward <yuri6037@outlook.com>
2022-11-20 20:42:03 +0100

    Initial Commit

---
+++ batch-sound-divider/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  batch-sound-divider
+//
+//  Created by Yuri Edward on 11/20/22.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
